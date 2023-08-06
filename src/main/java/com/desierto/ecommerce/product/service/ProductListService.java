778cb575f2ce8e52a3bec843c206bf56d7733857
jdesierto <desiertojl98@gmail.com>
2023-08-06 21:43:07 +0800

    Add Details View Feature
    - Added API to find product response using product ID.

--- src/main/java/com/desierto/ecommerce/product/service/ProductListService.java
+++ src/main/java/com/desierto/ecommerce/product/service/ProductListService.java
@@ -2,13 +2,15 @@ package com.desierto.ecommerce.product.service;
 
 import com.desierto.ecommerce.product.request.ProductListRequest;
 import com.desierto.ecommerce.product.request.ProductSearchRequest;
-import com.desierto.ecommerce.product.response.ProductListResponse;
+import com.desierto.ecommerce.product.response.ProductResponse;
 
 import java.util.List;
 
 public interface ProductListService {
 
-   List<ProductListResponse> getAllProducts(ProductListRequest request);
+   List<ProductResponse> getAllProducts(ProductListRequest request);
 
-   List<ProductListResponse> searchProductByKeyword(ProductSearchRequest request);
+   List<ProductResponse> searchProductByKeyword(ProductSearchRequest request);
+
+   ProductResponse getProductByProductId(Long id);
 }
