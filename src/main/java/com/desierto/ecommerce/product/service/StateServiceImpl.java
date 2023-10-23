4fa2094473bc5116beff54ac80a40c0dbc5ece4d
jdesierto <desiertojl98@gmail.com>
2023-10-23 23:02:23 +0800

    State API - Get all State and State by Country Id

---
+++ src/main/java/com/desierto/ecommerce/product/service/StateServiceImpl.java
@@ -0,0 +1,31 @@
+package com.desierto.ecommerce.product.service;
+
+import com.desierto.ecommerce.product.entity.State;
+import com.desierto.ecommerce.product.repository.StateRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class StateServiceImpl implements StateService {
+
+    private final StateRepository stateRepository;
+
+
+    @Override
+    public List<State> getAllStates() {
+        return stateRepository.findAll();
+    }
+
+    @Override
+    public List<State> getByCountryCode(Long countryId) {
+        return stateRepository.findByCountryId(countryId);
+    }
+}
+
